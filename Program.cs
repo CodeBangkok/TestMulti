ccd843ed60274f5723e1cf39f602df9fd7005435
Bond <surasuk@smartcontractthailand.com>
2019-05-08 10:34:58 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normal
+            for (int i=0; i<50;i++)
+            {
+                Console.Write(" ,{0}", i);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine();
+
+            // Parallel
+            Parallel.For(0, 50, i => {
+                Console.Write(" , {0}", i);
+                Thread.Sleep(200);
+            });
+        }
+    }
+}
